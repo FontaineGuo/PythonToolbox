9172f12255bf8ce47e1e5775f58dbc9e3fe7b050
Fontaine_Lab <fontaine_gzy@163.com>
2019-03-14 20:48:07 +0800

    file_tools

--- wow-addons-helper/dir_mng/file_tools.py
+++ wow-addons-helper/dir_mng/file_tools.py
@@ -1,4 +1,29 @@
-
+from hashlib import md5
 
 def checkMd5(srcFile, tgtFile):
-    pass
+    src_fp = open(srcFile, 'rb')
+    md5_src = md5(src_fp.read()).hexdigest()
+    src_fp.close()
+
+    tgt_fp = open(tgtFile, 'rb')
+    md5_tgt = md5(tgt_fp.read()).hexdigest()
+    tgt_fp.close()
+
+    if md5_src == md5_tgt:
+        # print("md5 check successed")
+        return True
+    else:
+        # print("md5 check failed")
+        return False
+
+
+    # with open(file_name) as file_to_check:
+    #     # read contents of the file
+    #     data = file_to_check.read()    
+    #     # pipe contents of the file through
+    #     md5_returned = hashlib.md5(data).hexdigest()
+
+
+# test
+# checkMd5("C:\\Users\\Fontaine\\Desktop\\Temp\\test.txt",
+#          "C:\\Users\\Fontaine\\Desktop\\Temp\\test\\test.txt")
