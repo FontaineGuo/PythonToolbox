993f8b7eab8181de960c72c34486392a0b633a72
Fontaine_470S_win10 <fontaine_gzy@163.com>
2019-03-19 23:53:37 +0800

    dir_general

--- wow-addons-helper/dir_mng/dir_general.py
+++ wow-addons-helper/dir_mng/dir_general.py
@@ -14,6 +14,7 @@ def copy_dir(src_dir, tgt_dir):
         s_file = os.path.join(src_dir,dir_file)
         t_file = os.path.join(tgt_dir,dir_file)
 
+
         # excute copy process if this is a file
         if os.path.isfile(s_file):
             if not os.path.exists(tgt_dir):
@@ -22,17 +23,17 @@ def copy_dir(src_dir, tgt_dir):
                 # check the MD5, if the result is the same
                 # do not copy this file
                 if not file_tools.checkMd5(s_file, t_file):
-                    shutil.copy(s_file, t_file)
-                    #yield s_file
+                   shutil.copy(s_file, t_file)
             else:
                 # if the file not exists , copy it directly
                 shutil.copy(s_file, t_file)
-                #yield s_file
+            yield s_file
 
 
         if os.path.isdir(s_file):
-            copy_dir(s_file, t_file)
-        
+            yield s_file
+            yield  from copy_dir(s_file, t_file)
+
 
 def del_dir(dir_path):
     shutil.rmtree(dir_path)
@@ -51,5 +52,14 @@ def check_wow_retail_path(dir_path):
 ## del_dir('C:\\Users\\Fontaine\\Desktop\\Temp\\Addons')
 ## copy_dir('C:\\Users\\Fontaine\\Desktop\\Temp\\SICP','C:\\Users\\Fontaine\\Desktop\\Temp\\teest_dir')
 ## check_wow_retail_path("G:\\TempStorge\\asd\\qwd\\World of Warcraft\\_retail_")
+# for s in copy_dir('C:\\Users\\Fonta\\Desktop\\Temp\\SICP','C:\\Users\\Fonta\\Desktop\\Temp\\teest_dir'):
+#     print(s)
+for s in copy_dir('C:\\Users\\Fonta\\Desktop\\Temp\\SICP', 'C:\\Users\\Fonta\\Desktop\\Temp\\teest_dir'):
+    print(s)
+
+
+
+
+
 
 
