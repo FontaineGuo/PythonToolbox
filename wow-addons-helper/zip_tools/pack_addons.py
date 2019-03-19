880eb9423ede3ecd547dccc994dd14d3f7d82b82
Fontaine_470S_win10 <fontaine_gzy@163.com>
2019-03-19 18:26:44 +0800

    pack addons

--- wow-addons-helper/zip_tools/pack_addons.py
+++ wow-addons-helper/zip_tools/pack_addons.py
@@ -4,12 +4,12 @@ import os
 
 folder_names = [r'Interface', r'WTF']
 
-def zip_dir(root_path, dirs, out_file_path):
+def zip_dir(root_path, out_file_path):
     zip_tar = zipfile.ZipFile(out_file_path, 'w', zipfile.ZIP_DEFLATED)
 
     parent_folder = root_path
 
-    for dir_p in dirs:
+    for dir_p in folder_names:
         dirpath = os.path.join(root_path, dir_p)
         for root, dirs, files in os.walk(dirpath):
             for dir_name in dirs:
@@ -20,6 +20,7 @@ def zip_dir(root_path, dirs, out_file_path):
             for file_name in files:
                 absolute_path = os.path.join(root_path,file_name)
                 out_path = os.path.join(out_file_path, file_name)
+                zip_tar.write(absolute_path, out_path)
                 print("Add '%s' to archive." % absolute_path)
 
         print('done')
