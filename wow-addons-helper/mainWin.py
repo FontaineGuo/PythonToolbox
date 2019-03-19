9b2e328b50360aad537fa9f53c44d2c74775f5ea
Fontaine_470S_win10 <fontaine_gzy@163.com>
2019-03-19 18:26:28 +0800

    mainUI

--- wow-addons-helper/mainWin.py
+++ wow-addons-helper/mainWin.py
@@ -1,32 +1,67 @@
 import sys
-import mainUI
-import json
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
-from PyQt5.QtWebEngineWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import  QPixmap
+import os
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog
+
 
 from mainUI import Ui_MainWindow
+
+from dir_mng import dir_general
+from zip_tools import extract_addons, pack_addons
+
+
 class AddonHelper(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(QMainWindow, self).__init__(parent)
         self.setupUi(self)
+        # <--------------------init the btn--------------------->
+        self.lb1_chose_btn_one.clicked.connect(self.chose_backup_path_btn_click)
+        self.lb1_chose_btn_two.clicked.connect(self.chose_backup_confirm_btn_click)
+
+        self.lb2_chose_btn_one.clicked.connect(self.chose_import_path_btn_click)
+        self.lb2_chose_btn_two.clicked.connect(self.chose_backup_confirm_btn_click)
+        # <----------------------------------------------------->
+
+        self.__backup_path = ""
+        self.__import_path = ""
 
     def chose_backup_path_btn_click(self):
-        # TODO: chose backup path
-        pass
+        dir_path =  str(QFileDialog.getExistingDirectory(self, 'Select Directory'))
+        # print(dir_path)
+        if dir_general.check_wow_retail_path(dir_path):
+            print("found the wow dir")
+            self.__backup_path = dir_path
+        else:
+            print("not found the wow dir")
+            QMessageBox.about(self, "Title", "Couldn't found the wow _retail_ folder")
+            self.__bakcup_path = ""
 
     def chose_backup_confirm_btn_click(self):
-        # TODO: excute zip func
-        pass
+        if self.__bakcup_path == "":
+            QMessageBox.about(self, "Title", "Please chose WOW folder")
+            return
+        pack_addons.zip_dir(self.__bakcup_path, r'Addons.zip')
 
     def chose_import_path_btn_click(self):
-        #TODO: chose import path
-        pass
+        dir_path = str(QFileDialog.getExistingDirectory(self, 'Select Directory'))
+        # print(dir_path)
+        if dir_general.check_wow_retail_path(dir_path):
+            print("found the wow dir")
+            self.__import_path = dir_path
+        else:
+            print("not found the wow dir")
+            QMessageBox.about(self, "Title", "Couldn't found the wow _retail_ folder")
+            self.__import_path = ""
 
     def chose_import_confirm_btn_click(self):
-        #TODO: excute copy_dir and flush the text area
-        pass
+        if self.__import_path == "":
+            QMessageBox.about(self, "Title", "Please chose WOW folder")
+            return
+        if not os.path.exists(r'Addons.zip'):
+            QMessageBox.about(self, "Title", "Couldn't found backup zip file 'Addons.zip'")
+            return
+        extract_addons.extract_package('.\\Addons', r'Addons.zip')
+        dir_general.copy_dir(".\\Addons",self.__import_path)
+
 
 # <-----------------------define the funcation of player data----------------------------------------------->
 
