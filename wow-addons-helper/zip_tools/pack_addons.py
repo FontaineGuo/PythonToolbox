2d9ada81b77ed0a22746de6d63fa1bed6c2e4b41
Fontaine_Lab <fontaine_gzy@163.com>
2019-03-20 16:52:48 +0800

    add log yield

--- wow-addons-helper/zip_tools/pack_addons.py
+++ wow-addons-helper/zip_tools/pack_addons.py
@@ -2,26 +2,32 @@ import zipfile
 import os
 
 
-folder_names = [r'Interface', r'WTF']
+_folder_names = [r'Interface', r'WTF']
 
 def zip_dir(root_path, out_file_path):
-    zip_tar = zipfile.ZipFile(out_file_path, 'w', zipfile.ZIP_DEFLATED)
+    print('processing zip')
+    zip_tgt = zipfile.ZipFile(out_file_path, 'w', zipfile.ZIP_DEFLATED)
 
     parent_folder = root_path
 
-    for dir_p in folder_names:
+    for dir_p in _folder_names:
         dirpath = os.path.join(root_path, dir_p)
         for root, dirs, files in os.walk(dirpath):
             for dir_name in dirs:
                 absolute_path = os.path.join(root, dir_name)
-                out_path = os.path.join(out_file_path, dir_name)
-                zip_tar.write(absolute_path, out_path)
-                print("Add '%s' to archive." % absolute_path)
+                relative_path = absolute_path.replace(parent_folder + '\\', '')
+                zip_tgt.write(absolute_path, relative_path)
+                # yield "Add " + absolute_path + " to archive."
             for file_name in files:
-                absolute_path = os.path.join(root_path,file_name)
-                out_path = os.path.join(out_file_path, file_name)
-                zip_tar.write(absolute_path, out_path)
-                print("Add '%s' to archive." % absolute_path)
+                absolute_path = os.path.join(root,file_name)
+                relative_path = absolute_path.replace(parent_folder + '\\', '')
+                zip_tgt.write(absolute_path, relative_path)
+                yield "Add " + absolute_path + " to archive."
 
-        print('done')
-        zip_tar.close()
+    # print('done')
+    zip_tgt.close()
+
+# for s in zip_dir("C:\\Users\\Fontaine\\Desktop\\Temp\\SICPs",
+#                  "C:\\Users\\Fontaine\\Desktop\\Temp\\Addons.zip"):
+#     print(s)
+
