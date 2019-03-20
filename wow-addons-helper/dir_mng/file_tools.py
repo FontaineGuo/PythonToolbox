27b78d27465d4bc47eb28c8754929685e357f0ee
Fontaine_Lab <fontaine_gzy@163.com>
2019-03-20 16:52:03 +0800

    add func
    format_path and count_files

--- wow-addons-helper/dir_mng/file_tools.py
+++ wow-addons-helper/dir_mng/file_tools.py
@@ -1,3 +1,4 @@
+import os
 from hashlib import md5
 
 def checkMd5(srcFile, tgtFile):
@@ -27,3 +28,20 @@ def checkMd5(srcFile, tgtFile):
 # test
 # checkMd5("C:\\Users\\Fontaine\\Desktop\\Temp\\test.txt",
 #          "C:\\Users\\Fontaine\\Desktop\\Temp\\test\\test.txt")
+
+def format_path(path):
+    # final_path = path.replace('/', '\\')
+    name_list = path.split('/')
+    final_path = name_list[0] + '\\'
+    for single_name in name_list[1:]:
+        final_path = os.path.join(final_path,single_name)
+    return final_path
+
+def count_files(path):
+    count = 0
+    for root,dirs, files in os.walk(path):
+        for f in files:
+            count += 1
+    return count
+
+# print(format_path("C:/Users/Fontaine/Desktop/Temp/World of Warcraft/_retail_"))
