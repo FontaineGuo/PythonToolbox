4964da73b88cf361ffc2c746b367cd6d29c448b8
Fontaine_470S_win10 <fontaine_gzy@163.com>
2019-04-02 18:23:27 +0800

    extract addons

--- wow-addons-helper/zip_tools/extract_addons.py
+++ wow-addons-helper/zip_tools/extract_addons.py
@@ -1,14 +1,28 @@
 import os
 import zipfile
-
-
+from dir_mng import dir_general
+import time
 
 
 def extract_package(path, path_of_addons):
     print("extract addons zip")
+    if not os.path.exists(path):
+        os.makedirs(path)
+    else:
+        dir_general.del_dir(path)
+        os.makedirs(path)
+
     with zipfile.ZipFile(path_of_addons, 'r') as zipf:
-        zipf.extractall(path=path)
+        for info in zipf.infolist():
+            yield info.filename
+            zipf.extract(info.filename, path)
+
     print("extract done")
 
-
+# start =  time.clock()
 # extract_package('.\\Addons', r'Addons.zip')
+# end = time.clock() - start
+# print("running time is -----" + str(start) + " seconds" )
+#
+# for info in extract_package('.\\Addons', r'Addons.zip'):
+#     print(info)
