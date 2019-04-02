8719d4d9eb7aaabe818ec4bb07a839d8baa619ad
Fontaine_Lab <fontaine_gzy@163.com>
2019-04-02 11:45:39 +0800

    optimize zip func

--- wow-addons-helper/zip_tools/extract_addons.py
+++ wow-addons-helper/zip_tools/extract_addons.py
@@ -5,8 +5,10 @@ import zipfile
 
 
 def extract_package(path, path_of_addons):
-    zipf = zipfile.ZipFile(path_of_addons)
     print("extract addons zip")
-    # if not os.path.exists(tempDir):
-    #                 os.makedirs(tempDir)
-    zipf.extractall(path = path)
+    with zipfile.ZipFile(path_of_addons, 'r') as zipf:
+        zipf.extractall(path=path)
+    print("extract done")
+
+
+# extract_package('.\\Addons', r'Addons.zip')
